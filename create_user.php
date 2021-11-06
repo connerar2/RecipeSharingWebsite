d12e4063b21e885a1227b42d6894e30d8c7883eb
Alec Conner <alec.conner.19@cnu.edu>
2021-11-06 19:20:42 -0400

    testing checking for same passwords when creating account

--- create_user.php
+++ create_user.php
@@ -2,21 +2,42 @@
 
 <html>
 	<head>
-		<h1>Create Account</h1>
+		<h1>Create Your Account</h1>
 	</head>
 	
 	<body>
-		<form method="post" action="new_user.php">
+		<form method="post" onsubmit="return validateForm();" action="new_user.php">
 			Enter Username:
 			<input type="text" name="username" placeholder="Enter Username" required>
 			<br>
 			Enter Password:
-			<input type="password" name="password" placeholder="Enter Password" required>
+			<input type="password" id="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Enter Password" required>
 			<br>
 			Re-Enter Password:
-			<input type="password" name="re_enter_password" placeholder="Enter Password" required>
+			<input type="password" id="check_password" name="re_enter_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Enter Password" required>
 			<br>
+			<p id="hidden" style="display: none">Passwords don't match</p>
 			<input type="submit" value="Create Account"><br>
 		</form>
 	</body>
+	
+	<script type="text/javascript">
+	
+	function samePasswords() {
+		var p1 = document.getElementByID("password");
+		var p2 = document.getElementByID("check_password");
+		
+		if (p1.value != p2.value) {
+			document.getElementByID("hidden").style.display("inline");
+			return false;
+		}
+		document.getElementByID("hidden").style.display("none");
+		return true;
+	}
+	
+	function validateForm() {
+		samePasswords();
+	}
+	
+	</script>
 </html>
--- login.php
+++ login.php
@@ -4,19 +4,7 @@
 	<head>
 		<h1>Login</h1>
 	</head>
-	<?php 
-		$host = "localhost";
-		$user = "root";
-		$password = "Happy124face1!";
-		$database = "tutorial";
-		
-		$cxn = mysqli_connect($host, $user, $password, $database);
-		
-		if (mysqli_connect_errno()) {
-			echo "Failed to connect to database: ".mysqli_connect_errno();
-			die();
-		}
-	?>
+	
 	<body>
 		<form method="post" action="logged_in.php">
 			Username:
