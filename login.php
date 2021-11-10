dd4408490878fc2a6ed7ce9c85140734e5bb5f96
Alec Conner <alec.conner.19@cnu.edu>
2021-11-10 17:47:03 -0500

    styled login page, will probably make more adjustments later

--- login.php
+++ login.php
@@ -8,29 +8,35 @@ include('logged_in.php');
 <!DOCTYPE HTML>
 
 <html>
+
+	<div class="login_page">
+			<div id="login_form">
 	<head>
 		<h1>Login</h1>
+		<link href='Styles/login_style.css' rel='stylesheet'>
 	</head>
 	
 	<body>
-		<form method="post" action="login.php">
-			Username:
-			<input type="text" name="username" placeholder="Enter Username" required>
-			<br/>
-			Password:
-			<input type="password" name="password" placeholder="Enter Password" required>
-			<br/>
-			<input type="submit" value="Login"><br>
-			<a href="create_user.php">Create a new account</a>
-			<br>
-		</form>
-		<?php
-			if (isset($_SESSION['username'])) {
-				echo "You have logged in!";
-			}
-			else {
-				
-			}
-		?>
+				<form method="post" action="login.php">
+					Username:
+					<input type="text" id="username" name="username" placeholder="Enter Username" required>
+					<br/>
+					Password:
+					<input type="password" id="password" name="password" placeholder="Enter Password" required>
+					<br/>
+					<input type="submit" value="Login"><br>
+					<a href="create_user.php">Create a new account</a>
+					<br>
+				</form>
+				<?php
+					if (isset($_SESSION['username'])) {
+						echo "You have logged in!";
+					}
+					else {
+						
+					}
+				?>
+			</div>
+		</div>
 	</body>
 </html>
