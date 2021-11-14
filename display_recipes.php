6b22c1d56d457833e977b0731d9ea517948cd776
Alec Conner <alec.conner.19@cnu.edu>
2021-11-14 15:11:52 -0500

    stlying buttons

--- display_recipes.php
+++ display_recipes.php
@@ -64,8 +64,8 @@
 		echo "</div>";
 		
 		echo "<div class=\"prev_and_next\">";
-			echo "<a id=\"next\" href=\"display_recipes.php?pn=".($pn + 1)."\">Next</a> ";
 			echo "<a id=\"previous\" href=\"display_recipes.php?pn=".($pn - 1)."\">Previous</a>";
+			echo "<a id=\"next\" href=\"display_recipes.php?pn=".($pn + 1)."\">Next</a> ";
 		echo "</div>";
 
 
