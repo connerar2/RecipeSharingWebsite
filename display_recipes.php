518e2c3d5a8838a59cf964a7f93049454a1ecb40
Alec Conner <alec.conner.19@cnu.edu>
2021-11-14 11:43:20 -0500

    forgot to add folder locaiton to path

--- display_recipes.php
+++ display_recipes.php
@@ -31,8 +31,7 @@
 		
 		while($row = $result->fetch_assoc()) {
 			echo "<div class=\"recipe\">";
-			echo $row['recipe_name'].".html";
-			echo "<a href=\"".$row['recipe_name'].".html\">".$row['recipe_name']."</a>";
+			echo "<a href=Recipes/\"".$row['recipe_name'].".html\">".$row['recipe_name']."</a>";
 				echo"<h3>".$row['recipe_name']."</h3>";
 				echo"<img class=\"recipeImage\" src=/".$row['meal_image']." alt=\"Image of the recipe\">";
 				echo "<div>";
