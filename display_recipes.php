00d1f015c222cefcbffc97585258c4c23486f8eb
Alec Conner <alec.conner.19@cnu.edu>
2021-11-20 17:52:34 -0500

    viewing possible pages

--- display_recipes.php
+++ display_recipes.php
@@ -30,9 +30,11 @@
 		
 		$possible_pages = ceil($rows->nums_rows/$results_per_page);
 		
-		echo "Num rows: ".$rows->num_rows."<br>";
+		$num_rows = $rows->nums_rows;
+		
+		echo "Num rows: ".$num_rows."<br>";
 		echo "Results Per Page: ".$results_per_page."<br>";
-		$num_page = $rows->nums_rows / $results_per_page;
+		$num_page = $num_rows / $results_per_page;
 		echo "Num Page: ".$num_page." 11/10 = ";
 		echo 11/10;
 		echo "<br>Ceil Result: ".ceil($rows->nums_rows/$results_per_page)."<br>";
