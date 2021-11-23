d7e297f45e66de7ca5275bba54cdd258cf039fd6
Alec Conner <alec.conner.19@cnu.edu>
2021-11-23 18:00:55 -0500

    possible final test

--- generate_recipe.php
+++ generate_recipe.php
@@ -125,7 +125,7 @@
 		$stmt->execute();
 		$result = $stmt->get_result();
 		
-		$id = $result->fetch_row()['id'] ?? false;
+		$id = $result->fetch_row()[0] ?? false;
 		
 		foreach ($ingredient_list as $ingredient) {
 			$possible_units = '/ (tsp|tbsp|oz|lb|cup|pinch|small|medium|large|gallon|quart|pint) /';
@@ -136,7 +136,7 @@
 			$stmt-> bind_param("s", strtolower($ingre[1]));
 			$stmt->execute();
 					
-			$stmt = $cxn->prepare("insert into recipe_ingredient (recipe_id, ingredient) values (?, ?)");
+			$stmt = $cxn->prepare("insert into recipe_ingredient (recipe_id, ingredient) values (?, '?')");
 			$stmt->bind_param("is", $id, strtolower($ingre[1]));
 			$stmt->execute();
 			
