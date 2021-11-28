51afac574c00f68ab8ebc446fc633eed9529e658
Alec Conner <alec.conner.19@cnu.edu>
2021-11-28 16:37:30 -0500

    login meesage appearing automatically

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 //start session
 session_start();
 
+include('logged_in.php');
 ?>
 
 <!DOCTYPE HTML>
@@ -38,48 +39,5 @@ session_start();
 			</div>
 		</div>
 	</body>
-	
-	<?php
-		if(isset($_POST['username'])) {
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		
-		
-		$host = "localhost";
-		$user = "root";
-		$db_password = "Happy124face1!";
-		$database = "tutorial";
-		
-		$cxn = mysqli_connect($host, $user, $db_password, $database);
-		
-		if (mysqli_connect_errno()) {
-			echo "Failed to connect to database: ".mysqli_connect_errno();
-			die();
-		}
-		
-		
-		$stmt = $cxn->prepare("SELECT * FROM users WHERE username = (?) ");
-		$stmt->bind_param("s", $username);
-		$stmt->execute();
-		
-		
-		$result = $stmt->get_result();
-		$user = $result->fetch_assoc();
-		
-		if(password_verify($password, $user['password'])) {
-			$_SESSION["username"]= $username;
-			echo "<script>";
-			echo "document.getElementById(\"login_message\").innerHTML = \"You have logged in!\"\n";
-			echo "</script>";
-		}	
-		else {
-			echo "<script>";
-			echo "document.getElementById(\"login_message\").innerHTML = \"Wrong username or password\"\n";
-			echo "</script>";
-		}
-		
-		$cxn->close();
-		}
-	?>
 
 </html>
