4729d558745ad21bf19ba3546c99cdae66ee5b81
Alec Conner <alec.conner.19@cnu.edu>
2021-11-28 16:36:57 -0500

    login meesage appearing automatically

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 //start session
 session_start();
 
-include('logged_in.php');
 ?>
 
 <!DOCTYPE HTML>
@@ -36,26 +35,51 @@ include('logged_in.php');
 					<a href="create_user.php">Create an Account</a>
 					<br>
 				</form>
-				<?php
-					//if (isset($_SESSION['username'])) {
-						//echo "<script\n>";
-						//echo "document.getElementById(\"login_message\").innerHTML = \"You have logged in!\"\n";
-						//echo "document.getElementById(\"login_fail\").innerHTML = \"\"\n";
-						//echo "</script>\n";
-					//}
-					//else {
-						//echo "<script>\n";
-						//echo "if (document.getElementById(\"login_message\").innerHTML = \"\") {\n";
-						//echo "document.getElementById(\"login_message\").innerHTML = \" \"\n";
-						//echo "}\n";
-						//echo "else {\n";
-						//echo "document.getElementById(\"login_fail\").innerHTML = \"Wrong username or password\"\n";
-						//echo "}\n";
-						//echo "</script>\n";
-					//}
-				?>
 			</div>
 		</div>
 	</body>
+	
+	<?php
+		if(isset($_POST['username'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		
+		
+		$host = "localhost";
+		$user = "root";
+		$db_password = "Happy124face1!";
+		$database = "tutorial";
+		
+		$cxn = mysqli_connect($host, $user, $db_password, $database);
+		
+		if (mysqli_connect_errno()) {
+			echo "Failed to connect to database: ".mysqli_connect_errno();
+			die();
+		}
+		
+		
+		$stmt = $cxn->prepare("SELECT * FROM users WHERE username = (?) ");
+		$stmt->bind_param("s", $username);
+		$stmt->execute();
+		
+		
+		$result = $stmt->get_result();
+		$user = $result->fetch_assoc();
+		
+		if(password_verify($password, $user['password'])) {
+			$_SESSION["username"]= $username;
+			echo "<script>";
+			echo "document.getElementById(\"login_message\").innerHTML = \"You have logged in!\"\n";
+			echo "</script>";
+		}	
+		else {
+			echo "<script>";
+			echo "document.getElementById(\"login_message\").innerHTML = \"Wrong username or password\"\n";
+			echo "</script>";
+		}
+		
+		$cxn->close();
+		}
+	?>
 
 </html>
