ad674dc5d21e91da8a13809a3868357132c23b2f
Alec Conner <alec.conner.19@cnu.edu>
2021-11-28 14:38:26 -0500

    working on fail to login message

--- login.php
+++ login.php
@@ -32,21 +32,26 @@ include('logged_in.php');
 					<input type="password" id="password" name="password" placeholder="Enter Password" required>
 					<br/>
 					<input type="submit" value="Login"><br>
+					<p id="message"></p>
 					<a href="create_user.php">Create an Account</a>
 					<br>
 				</form>
 				<?php
 					if (isset($_SESSION['username'])) {
-						echo "You have logged in!";
-						//echo "<script>";
-						//echo "document.getElementById(\"error\").innerHTML = \"You have logged in!\"";
-						//echo "</script>";
+						echo "<script>";
+						echo "document.getElementById(\"message\").innerHTML = \"You have logged in!\"";
+						//echo "document.getElementById(\"error\").removeAttribute(\"hidden\")";
+						echo "</script>";
 					}
 					else {
-						echo "Wrong username or password";
-						//echo "<script>";
-						//echo "document.getElementById(\"error\").innerHTML = \"Wrong username or password\"";
-						//echo "</script>";
+						echo "<script>";
+						echo "if (document.getElementById(\"message\").innerHTML = \"\") {";
+						echo "document.getElementById(\"message\").innerHTML = \" \"";
+						echo "}";
+						echo "else {";
+						echo "document.getElementById(\"message\").innerHTML = \"Wrong username or password\"";
+						echo "}";
+						echo "</script>";
 					}
 				?>
 			</div>
