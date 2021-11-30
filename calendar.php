cf69446319b069c360334c4d038f88d6fe0ba267
Alec Conner <alec.conner.19@cnu.edu>
2021-11-30 00:15:10 -0500

    testing add recipe

--- calendar.php
+++ calendar.php
@@ -43,7 +43,7 @@
 					echo "possibleDays[i].innerHTML = ".$day." + \" <a href=\"".$link."\">Name</a>";
 					echo "}"
 					echo "else {";
-					echo "//possibleDays[i].innerHTML += \" <a href=\"".$link."\">Name</a>";
+					echo "possibleDays[i].innerHTML += \" <a href=\"".$link."\">Name</a>";
 					echo "}";
 					
 					
